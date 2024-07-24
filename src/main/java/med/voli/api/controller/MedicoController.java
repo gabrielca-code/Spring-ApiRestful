96258e562a6b8a5ceb8dee5207a8e82cd10708f3
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-07-24 20:37:33 -0300

    Criando metodo get para listar medicos

--- src/main/java/med/voli/api/controller/MedicoController.java
+++ src/main/java/med/voli/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package med.voli.api.controller;
 
 import jakarta.validation.Valid;
+import med.voli.api.medico.DadosListagemMedico;
 import med.voli.api.medico.Medico;
 import med.voli.api.medico.MedicoCadastrarRecord;
 import med.voli.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid MedicoCadastrarRecord dadosMedico) {
         medicoRepository.save(new Medico(dadosMedico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
