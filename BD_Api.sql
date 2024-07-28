d1609bfdbf8efb995159e3db83e7a0cd3d4562bb
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-07-28 17:14:02 -0300

    Configurando autenticação do usuário

--- BD_Api.sql
+++ BD_Api.sql
@@ -2,3 +2,5 @@ create database vollmed_api;
 use vollmed_api;
 
 select * from medicos;
+
+insert into usuarios values(1, "g@gmail.com", "123456");
---
+++ src/main/java/med/voli/api/controller/AutenticacaoController.java
@@ -0,0 +1,30 @@
+package med.voli.api.controller;
+
+import jakarta.validation.Valid;
+import med.voli.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
