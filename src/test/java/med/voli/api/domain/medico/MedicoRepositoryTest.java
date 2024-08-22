4b0a775704d468dcd20f60d025c993f8107800ae
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-08-22 08:03:15 -0300

    Iniciando os testes da aplicação

---
+++ src/test/java/med/voli/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,20 @@
+package med.voli.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest //usado para testar interfaces repositorys
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+
+    }
+
+}
