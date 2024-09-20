0e273c75f49fcb4e8141d7dfad556d758a6f9885
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-09-19 21:11:39 -0300

    Preparação para deploy da aplicação

--- src/main/java/med/voli/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voli/api/domain/usuario/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package med.voli.api.domain.usuario;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> { //define uma classe da camada de repository, que fará as chamadas com o BD
                                                                             //para isso, necessário extender JpaRepository (que ja possui a annotation @Repository, por isso n se faz necessária
                                                                             //Passando Classe e o tipo do ID da classe
